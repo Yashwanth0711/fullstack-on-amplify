90cd483f6c882a29fa2ad53c544bcdc2c87b0e15
Yashwanth0711 <Yashwanthyash07112@gmail.com>
2024-01-30 00:05:59 +0530

    added graphQL api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullstackonamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "fullstackonamplify": {
       "AppClientID": "string",
